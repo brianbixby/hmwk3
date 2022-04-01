ba56070b7c3e923d0401cab357d18d0cc1550e07
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-03-31 17:54:41 -0700

    done with password logic, except question about special charachters

--- script.js
+++ script.js
@@ -31,8 +31,8 @@ function charachterPrompt(charType, retry = false) {
 }
 
 function generatePassword() {
-  //TODO: your code goes here
-  let length, lowercase, uppercase, numeric, specialChars, resubmit;
+  // PROMPTS
+  let length, lowercase, uppercase, numeric, specialChars;
   length = lengthPrompt();
   if (length === null) return '';
   lowercase = charachterPrompt("lowercase letters");
@@ -45,10 +45,41 @@ function generatePassword() {
   if (specialChars === null) return '';
   console.log("length:", length, "lowercase:", lowercase, "uppercase:", uppercase, "numeric:", numeric, "specialChars:", specialChars);
   if (!lowercase && !uppercase && !numeric && !specialChars) {
-    resubmit = window.confirm("At least one character type must be selected (lowercase, uppercase, numeric or special chachters). Please click okay if you would like to try again.");
-    console.log("resubmit: ", resubmit);
+    let resubmit = window.confirm("At least one character type must be selected (lowercase, uppercase, numeric or special chachters). Please click okay if you would like to try again.");
     return resubmit ? generatePassword() : '';
   }
+  // PASSWORD LOGIC
+  var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialCharsArray = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~", "]"];
+  var charachtersArray = [];
+  var password = "";
+
+  if (lowercase) {
+    password += lowercaseArray[Math.floor(Math.random() * 26)];
+    charachtersArray = [...lowercaseArray];
+    length--;
+  }
+  if (uppercase) {
+    password += uppercaseArray[Math.floor(Math.random() * 26)];
+    charachtersArray = [...charachtersArray, ...uppercaseArray];
+    length--;
+  }
+  if (numeric) {
+    password += numericArray[Math.floor(Math.random() * 10)];
+    charachtersArray = [...charachtersArray, ...numericArray];
+    length--;
+  }
+  if (specialChars) {
+    password += specialCharsArray[Math.floor(Math.random() * specialCharsArray.length)];
+    charachtersArray = [...charachtersArray, ...specialCharsArray];
+    length--;
+  }
+  while (--length >= 0) {
+    password += charachtersArray[Math.floor(Math.random() * charachtersArray.length)];
+  }
+  return password;
 }
 
 
